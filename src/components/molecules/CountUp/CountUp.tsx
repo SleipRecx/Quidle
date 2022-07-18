f93d9b12b03cb63c0fc247415273b23227992d54
erikkjernlie <erik.kjernlie@gmail.com>
2022-07-18 11:23:22 +0200

    Update scores

--- src/components/molecules/CountUp/CountUp.tsx
+++ src/components/molecules/CountUp/CountUp.tsx
@@ -2,7 +2,7 @@ import ReactCountUp from "react-countup";
 import { CountUpProps } from "./types";
 
 const CountUp = ({ end }: CountUpProps) => {
-  return <ReactCountUp end={end} duration={0.2} />;
+  return <ReactCountUp end={end} duration={0.4} />;
 };
 
 export default CountUp;
