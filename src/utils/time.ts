8663da5186d72405dba2946dee2e2b08b15779a9
erikkjernlie <erik.kjernlie@gmail.com>
2022-07-20 20:35:59 +0200

    Update for today

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,10 +1,10 @@
 export const getTodaysDate = (): string => {
   // YYYY-MM-DD
 
-  //
+  /*
   var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 2);
-  return tomorrow.toISOString().split("T")[0];
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  return tomorrow.toISOString().split("T")[0];*/
 
   return new Date().toISOString().split("T")[0];
 };
