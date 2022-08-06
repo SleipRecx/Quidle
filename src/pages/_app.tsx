d3640b5f83de64fa5594ad4f4bab4e917dafc3ae
erikkjernlie <erik.kjernlie@gmail.com>
2022-08-06 10:28:07 +0200

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
 import "aos/dist/aos.css";
 import AOS, { AosOptions } from "aos";
+import Script from "next/script";
 
 const AOS_SETTINGS: AosOptions = {
   easing: "ease",
@@ -17,6 +18,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="123">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Component {...pageProps} />
       <Toaster />
     </>
