f2285beba26ce42e919b90ad77a69d1ebf9eabfa
erikkjernlie <erik.kjernlie@gmail.com>
2022-08-15 21:19:38 +0200

    Update time

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -8,7 +8,7 @@ export const getTodaysDate = (): string => {
   let myDate = new Date();
   const offset = new Date().getTimezoneOffset();
   myDate = new Date(myDate.getTime() - offset * 60 * 1000);
-  return "2022-08-16";
+  return myDate.toISOString().split("T")[0]; // "2022-08-23" write
 };
 
 export const getTodaysDateDDMM = (): string => {
